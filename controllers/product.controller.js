ebbe9ca95b90cd72caa2b25f9e28d2e7c02c4844
Victor Varghese <victor.varghese@neudesic.com>
2025-04-21 12:44:25 +0530

    Update product.controller.js

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -50,7 +50,21 @@ const updateProduct = async (req, res) => {
   }
 };
 
-const deleteProduct = async (req, res) => {};
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+
+    // check
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 module.exports = {
   getProducts,
